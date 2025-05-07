b8368a214e42a2b5d2438c3a23a7a944b746e83d
0xcrypto102 <tiensucha5@gmail.com>
2025-05-07 10:23:46 -0700

    create a newInterchainToken.js script

---
+++ newInterchainToken.js
@@ -0,0 +1,18 @@
+const hre = require("hardhat");
+const crypto = require("crypto");
+const ethers = hre.ethers;
+const {
+  AxelarQueryAPI,
+  Environment,
+  EvmChain,
+  GasToken,
+} = require("@axelar-network/axelarjs-sdk");
+
+const interchainTokenServiceContractABI = require("./utils/interchainTokenServiceABI");
+const interchainTokenFactoryContractABI = require("./utils/interchainTokenFactoryABI");
+const interchainTokenContractABI = require("./utils/interchainTokenABI");
+
+const interchainTokenServiceContractAddress =
+  "0xB5FB4BE02232B1bBA4dC8f81dc24C26980dE9e3C";
+const interchainTokenFactoryContractAddress =
+  "0x83a93500d23Fbc3e82B410aD07A6a9F7A0670D66";
